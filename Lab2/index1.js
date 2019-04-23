7715c42ffca63dd63608275810846c5449e6aa6f
lyman l shen <lshen@linux60811.dc.engr.scu.edu>
2019-04-23 12:34:31 -0700

    Button changes size appropiately on mobile devices

--- Lab2/index1.js
+++ Lab2/index1.js
@@ -4,10 +4,16 @@ function toggleNav() {
   if(document.getElementById("nav-link").style.display == "none") {
     document.getElementById("nav-link").style.display = "block";
     document.getElementById("nav-bar").style.height = "auto";
-    document.getElementById("button").src = "Button-02.png";
+    buttonImg = document.getElementById("button");
+    buttonImg.src = "Button-02.png";
+    buttonImg.style.top = "15px";
+    buttonImg.style.height = "20px";
   } else {
     document.getElementById("nav-link").style.display = "none";
     document.getElementById("nav-bar").style.height = "3px";
-    document.getElementById("button").src = "Button-01.png";
+    buttonImg = document.getElementById("button");
+    buttonImg.src = "Button-01.png";
+    buttonImg.style.top = "10px";
+    buttonImg.style.height = "30px";
   }
 }
