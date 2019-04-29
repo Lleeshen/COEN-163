e5d0a5274519db3f536f5725669737b52e03de77
lyman l shen <lshen@dcmac10.dc.engr.scu.edu>
2019-04-29 15:35:51 -0700

    Added comments for not immediately logical sections

--- Lab2/index1.js
+++ Lab2/index1.js
@@ -2,15 +2,21 @@ window.onresize = function(){ location.reload(); }
 
 function toggleNav() {
   buttonImg = document.getElementById("button");
+  //Styling when button is open
   if(document.getElementById("nav-link").style.display == "none") {
+    //Show the navigation links
     document.getElementById("nav-link").style.display = "block";
     document.getElementById("nav-bar").style.height = "auto";
+    //Use smaller close nav button
     buttonImg.src = "Button-02.png";
     buttonImg.style.top = "15px";
     buttonImg.style.height = "20px";
   } else {
+  //Styling when button is closed
+    //Hide navigation links, keep black line
     document.getElementById("nav-link").style.display = "none";
     document.getElementById("nav-bar").style.height = "3px";
+    //Use larger open nav button
     buttonImg.src = "Button-01.png";
     buttonImg.style.top = "10px";
     buttonImg.style.height = "30px";
--- Lab2/index2.js
+++ Lab2/index2.js
@@ -2,18 +2,24 @@ window.onresize = function(){ location.reload(); }
 
 function toggleNav() {
   buttonImg = document.getElementById("button");
+  //Styling when menu is open
   if(buttonImg.style.top == "10px") {
+    //Open side nav bar and move page to the left
     document.getElementById("sideNav").style.width = "160px";
     document.getElementById("main").style.right= "150px";
     document.getElementById("footer").style.left= "150px";
+    //Switch to close button, make it smaller
     buttonImg.src = "Button-02.png";
     buttonImg.style.top = "15px";
     buttonImg.style.height = "20px";
     buttonImg.style.left = "13px";
+  //Styling when menu is closed
   } else {
+    //Close side nav bar and reset page width
     document.getElementById("sideNav").style.width = "0";
     document.getElementById("main").style.right= "0";
     document.getElementById("footer").style.left= "0";
+    //Switch to open button, make it larger
     buttonImg.src = "Button-01.png";
     buttonImg.style.top = "10px";
     buttonImg.style.height = "30px";
